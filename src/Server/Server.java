c30fd3265c89095a93ecb021860937fef563fdbf
Fabian Maximilian Spielberg <fabian.m.spielberg@gmail.com>
2024-06-12 20:34:07 +0200

    added docstrings to Server.java

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -8,12 +8,27 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Connects to a server.
+ * Used to transport data such as player data to the server where it will be stored and processed.
+ * Required for online play.
+ */
 public class Server {
+    /**
+     * The internal connection.
+     */
     private final Connection connection;
+    /**
+     * The logger for the Server module.
+     */
     private final Logger logger = new Logger("Server");
 
     private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+    /**
+     * The {@link MessageDigest}, that can hash byte arrays with <a href="https://wikipedia.org/wiki/SHA-2">SHA-256</a>.
+     */
     private static final MessageDigest md;
+    // required for to get the SHA-256 hash.
     static {
         try {
             md = MessageDigest.getInstance("SHA-256");
@@ -21,6 +36,13 @@ public class Server {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * Constructor for {@link Server}.
+     * @param host The ip or url of the server to connect to.
+     * @param port The port of the server. (e.g. 8080)
+     * @throws IOException When an IOException occurs, most likely because it couldn't connect to the server.
+     */
     public Server(String host, int port) throws IOException {
         this.connection = new Connection(host, port);
     }
@@ -36,12 +58,26 @@ public class Server {
         this.connection.login(username, hash);
         this.connection.start();
     }
+
+    /**
+     * Similar to {@link #login}, but creates a new account.
+     * @param username The name of the player.
+     * @param password The password to the account.
+     * @throws IOException
+     */
     public void signup(String username, String password) throws IOException {
         String hash = bytesToHex(md.digest(password.getBytes()));
         logger.debug("signing up with hash: " + hash);
         this.connection.signup(username, hash);
         this.connection.start();
     };
+
+    /**
+     * Helper function for {@link #login} and {@link #signup}, because the passwords are hashed (not transmitting and storing plaintext passwords, duh).
+     * {@link MessageDigest#digest} however returns a byte array that has to be turned into a string, and I chose a hex-string.
+     * @param bytes The bytes to be turned into hex.
+     * @return The hexadecimal representation of the bytes.
+     */
     private static String bytesToHex(byte[] bytes) {
         byte[] hexChars = new byte[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
